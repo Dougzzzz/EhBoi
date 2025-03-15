a6f11936c773b85ba6c02600d0ae969c65136a76
dougzzzz <douglasbarcelos02@gmail.com>
2025-03-15 08:36:08 -0300

    Criado classe de teste base para os testes unitarios, incluida referencia ao projeto de teste dos projetos Dominio e Infra

---
+++ EhBoi.Test/TesteBase.cs
@@ -0,0 +1,28 @@
+﻿using EhBoi.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EhBoi.Test
+{
+    public class TesteBase : IDisposable
+    {
+        protected ServiceProvider _serviceProvider;
+        public TesteBase()
+        {
+            var services = new ServiceCollection();
+            services.AddDbContext<EhBoiDbContext>(options =>
+            {
+                options.UseInMemoryDatabase(Guid.NewGuid().ToString());
+            });
+
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        public void Dispose()
+        {
+            _serviceProvider.Dispose();
+        }
+
+    }
+
+}
